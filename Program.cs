11465e110009956e44ac6696cb7c91d9b6f76c56
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-19 15:18:48 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace UserInput__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String X;
+            
+            Console.Write("Masukkan Input Kalimat : ");
+            X = Console.ReadLine();
+            Console.WriteLine("\nAnda Menulis = '{0}'\n\n", X);
+
+            Console.ReadLine();
+        }
+    }
+}
